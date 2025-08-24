0fd16240d8c4c77eaa47fb20ca55efb9a431bf4d
titaniumsquirrel <leocpugh@gmail.com>
2025-08-24 11:19:11 +0000

    commit

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-console.log("Portfolio site loaded successfully!");
+function showSection(id) {
+  // Hide all sections
+  document.querySelectorAll("main section").forEach((section) => {
+    section.classList.remove("active");
+  });
 
-
-
-
+  // Show clicked section
+  document.getElementById(id).classList.add("active");
+}
